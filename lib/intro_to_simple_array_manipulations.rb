9c94ed9b1fb5a55497a2e3adf6d81cca3408741f
Makeda P Mckune <makedamckune@gmail.com>
2019-09-30 06:56:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,16 +2,17 @@ colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
 
 colors_in_the_rainbow.push("violet")
 
-    end
+p colors_in_the_rainbow
+
 end
 
-describe "using_unshift" do 
-  before(:each) do 
-    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-    @new_neighborhood = "Staten Island"
-    @updated_array = using_unshift(bouroughs_in_nyc, @new_neighborhood)
-  end
+
+
+bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+
+bouroughs_in_nyc.unshift("Staten Island")
+
+p bouroughs_in_nyc
+   
+end
   
-  it "takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method" do
-    expect(@updated_array.first).to eq(@new_neighborhood)
-  end
