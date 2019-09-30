226927dbe276b60dc9cea85a0ef0ccd8dcb82f41
Makeda P Mckune <makedamckune@gmail.com>
2019-09-30 06:51:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "violet"
+     = "violet"
     updated_array = using_push(colors_in_the_rainbow, next_color)
     expect(updated_array.last).to eq(next_color)
     end
